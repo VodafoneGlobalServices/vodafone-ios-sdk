5a711c0c03a3865d3040c83058cbd35c0225b71c
Michał Szymańczyk <michal.szymancyk@mobica.com>
2014-07-01 16:17:30 +0200

    Initial project files

---
+++ HeApiIOsSdk/HeApiIOsSdk.h
@@ -0,0 +1,13 @@
+//
+//  HeApiIOsSdk.h
+//  HeApiIOsSdk
+//
+//  Created by Michał Szymańczyk on 01/07/14.
+//  Copyright (c) 2014 VOD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HeApiIOsSdk : NSObject
+
+@end

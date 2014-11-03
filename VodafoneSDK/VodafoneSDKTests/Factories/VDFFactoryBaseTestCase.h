0ccaadc750f0db1d86b8afa59ae34d116bf98551
Michał Szymańczyk <michal.szymancyk@mobica.com>
2014-11-03 10:46:34 +0100

    base factory test case

--- VodafoneSDK/VodafoneSDKTests/Factories/VDFFactoryBaseTestCase.h
+++ VodafoneSDK/VodafoneSDKTests/Factories/VDFFactoryBaseTestCase.h
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 #import "VDFArrayObserversContainer.h"
 #import "VDFHttpConnector.h"
+#import "VDFTestCase.h"
 
 @interface VDFHttpConnector ()
 @property (nonatomic, assign) id<VDFHttpConnectorDelegate> delegate;
@@ -19,7 +20,7 @@
 @property NSMutableArray *observers;
 @end
 
-@interface VDFFactoryBaseTestCase : XCTestCase
+@interface VDFFactoryBaseTestCase : VDFTestCase
 
 - (id)runAndAssertSimpleCreateMethodOnTarget:(id)target selector:(SEL)selector expectedResultClass:(Class)expectedResultClass;
 
--- VodafoneSDK/VodafoneSDKTests/Factories/VDFFactoryBaseTestCase.m
+++ VodafoneSDK/VodafoneSDKTests/Factories/VDFFactoryBaseTestCase.m
@@ -9,22 +9,7 @@
 #import "VDFFactoryBaseTestCase.h"
 #import <XCTest/XCTest.h>
 
-extern void __gcov_flush();
-
 @implementation VDFFactoryBaseTestCase
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    __gcov_flush();
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
 
 
 - (id)runAndAssertSimpleCreateMethodOnTarget:(id)target selector:(SEL)selector expectedResultClass:(Class)expectedResultClass {
@@ -37,6 +22,5 @@ extern void __gcov_flush();
     
     return result;
 }
-
 
 @end

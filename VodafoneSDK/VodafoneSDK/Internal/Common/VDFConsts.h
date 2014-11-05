fbda0ba016fbcedb7e957ccf16a4f5df88dad0a6
Michał Szymańczyk <michal.szymancyk@mobica.com>
2014-11-05 11:55:17 +0100

    SDK version for release

--- VodafoneSDK/VodafoneSDK/Internal/Common/VDFConsts.h
+++ VodafoneSDK/VodafoneSDK/Internal/Common/VDFConsts.h
@@ -10,7 +10,7 @@
 #define VodafoneSDK_VDFConsts_h
 
 // current version of SDK
-#define VDF_IOS_SDK_VERSION_STRING @"0.0.2"
+#define VDF_IOS_SDK_VERSION_STRING @"1.0.0"
 
 #pragma mark - Configuration consts:
 
